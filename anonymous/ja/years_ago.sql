f3ac5158d475fea1a3857629cdd2fb5acb1e000a
akahori <akahori418@gmail.com>
2023-09-01 12:55:29 +0900

    delete ja

--- anonymous/ja/years_ago.sql
+++
@@ -1,9 +0,0 @@
-DROP FUNCTION IF EXISTS years_ago(INTEGER);
-
-CREATE OR REPLACE FUNCTION years_ago(INTEGER)
-RETURNS INTEGER AS $$
-  SELECT (extract(year from current_date)::INTEGER - first_appeared)
-  FROM programming
-  WHERE id = $1;
-  $$ LANGUAGE sql;
-
